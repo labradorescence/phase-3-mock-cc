b9a990c1e1a9a08868fea3b886add3ffbdd887d7
Roy Lee <leeroy0912@gmail.com>
2022-09-28 11:23:40 -0400

    finished all the tests

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie (dev,item_name,value)
+        Freebie.create(item_name: item_name, dev_id: dev.id, company_id: self.id, value:value)
+    end
+
+    def self.oldest_company
+        Company.all.sort_by{|company| company.founding_year}.first
+    end
 end
